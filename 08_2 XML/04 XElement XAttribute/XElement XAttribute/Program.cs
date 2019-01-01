1732c0937b3a259d49594a527e4e7e7a551e23ab
Lab4 RUPPIN <lab4@ruppin.ac.il>
2019-01-01 08:31:19 +0200

    XML

---
+++ 08_2 XML/04 XElement XAttribute/XElement XAttribute/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XElement_XAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteXmlFile();
+
+            //PrintPersons();
+
+            //PrintStudents();
+
+            //ChangeStudents();
+
+            Console.ReadLine();
+        }
+
+        private static void ChangeStudents()
+        {
+            XDocument StudentDocument = XDocument.Load("Students.xml");
+            XElement studentsRoot = StudentDocument.Root;
+            IEnumerable<XElement> students = studentsRoot.Elements("person");
+            
+            IEnumerable<XElement> grades = students.Elements("Grade");
+            Console.WriteLine("\nGRADES:");
+
+            //option1
+            foreach (var grade in grades)
+            {
+                //grade = new XElement("Grade",100); //ERROR because the ienumerator interface is read only (but not for inner properties!)
+                Console.WriteLine(grade.Value);
+                grade.Value = "100";
+                Console.WriteLine(grade.Value);
+            }
+
+            //option2
+            //foreach (var item in students)
+            //{
+            //    foreach (var g in item.Elements("Grade"))
+            //    {
+            //        g.Value = "100";    
+            //    }
+            //}
+
+            StudentDocument.Save("Students after changes.xml");
+        }
+
+        private static void PrintStudents()
+        {
+            XDocument StudentDocument = XDocument.Load("Students.xml");
+            XElement studentsRoot = StudentDocument.Root;
+            IEnumerable<XElement> students = studentsRoot.Elements("person");
+            IEnumerable<XElement> names = students.Elements("Name");
+            
+            //this will return an empty collection because the name element is not under the root element at all!
+            IEnumerable<XElement> names2 = studentsRoot.Elements("Name");
+
+            Console.WriteLine("\nNAMES:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name.Value);
+            }
+
+
+            IEnumerable<XElement> grades = students.Elements("Grade");
+            Console.WriteLine("\nGRADES:");
+
+            foreach (var grade in grades)
+            {
+                Console.WriteLine(grade.Value);
+            }
+
+
+            Console.WriteLine("\nNAMES AND GRADES:");
+            foreach (XElement student in students)
+            {
+                Console.WriteLine("\n" + student.Element("Name").Value);
+                foreach (XElement grade in student.Elements("Grade"))
+                {
+                    Console.WriteLine("\t" + grade.Attribute("course").Value + ": " + grade.Value);
+                }
+            }
+        }
+
+        private static void PrintPersons()
+        {
+            XDocument personsXml = XDocument.Load("Perons.xml");
+            //print all the xml
+            Console.WriteLine(personsXml);
+            Console.WriteLine("\n___________________________\n");
+
+            //XName asd =  XName.Get("Name");
+            XElement root = personsXml.Root;
+            IEnumerable<XElement> persons = root.Elements();
+
+          
+            Console.WriteLine();
+            foreach (var person in persons)
+            {
+                if (int.Parse((person.Attribute("Age").Value)) > 20)
+                {
+                    Console.WriteLine(person);    
+                }
+                
+            }
+        }
+
+        private static void WriteXmlFile()
+        {
+            XDocument persons = new XDocument(
+                new XElement("persons",
+                    new XElement("person",
+                        new XElement("Name", "chipopo"),
+                        new XElement("Address", "street one 7"),
+                        new XAttribute("Age", 32)),
+                    new XElement("person",
+                        new XElement("Name", "yakov"),
+                        new XElement("Address", "street two 2"),
+                        new XAttribute("Age", 20)),
+                    new XElement("person",
+                        new XElement("Name", "sarit"),
+                        new XElement("Address", "street sham 40"),
+                        new XAttribute("Age", 3)),
+                    new XElement("person",
+                        new XElement("Name", "rona"),
+                        new XElement("Address", "street po 10"),
+                        new XAttribute("Age", 23))
+                )
+            );
+
+            persons.Save("Perons.xml");
+
+            XDocument studentsDocument = persons;
+            IEnumerable<XElement> students = studentsDocument.Elements();
+            int gr = 70;
+            foreach (var student in students.Elements())
+            {
+                student.Add(
+                    new XElement("Grade", gr+=5,
+                    new XAttribute("course", "math")),
+                    new XElement("Grade", gr ,
+                    new XAttribute("course", "eng"))
+                    );
+            }
+
+            studentsDocument.Save("Students.xml");
+        }
+    }
+}
---
+++ 08_2 XML/07 XPath/XPath/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+namespace XPath
+{
+    class Program
+    {
+        static XmlDocument doc;
+        static void Main(string[] args)
+        {
+            doc = new XmlDocument();
+            string xPathExp= string.Empty;
+            try
+            {
+                doc.Load("Books.xml");
+
+                //select root element
+                 XmlNode nd =  doc.SelectSingleNode("/");
+                
+
+                //selct all element with name
+                 xPathExp = "/books/book";
+                 PrintResults(xPathExp);
+                //select price
+                 Console.WriteLine("\nselect price");
+                 xPathExp = "/books/book/price";
+                 PrintResults(xPathExp);
+
+
+                //all authors last name - Attribute
+                 Console.WriteLine("\nall authors last name - Attribute");
+                 xPathExp = "/books/book/authors/author/@lname";
+                 PrintResults(xPathExp);
+
+                 Console.WriteLine("\n title of first book");
+                //title of first book
+                 xPathExp = "/books/book[1]/title";
+                 PrintResults(xPathExp);
+
+                //last
+                 Console.WriteLine("\n title of last book");
+
+                 xPathExp = "/books/book[last()]/title";
+                 PrintResults(xPathExp);
+
+                 Console.WriteLine("\n title of position book");
+                 xPathExp = "/books/book[position() = last() - 1]/title";
+                 PrintResults(xPathExp);
+
+                Console.WriteLine("\n conditions");
+                xPathExp = "/books/book[price>20]/title";
+                 PrintResults(xPathExp);
+
+              
+
+                /*
+                Operators :
+         "/books/book/title | /books/book/price" - All book titles and prices 
+          "/books/book[count(authors/author) >= 3]">Books with >=3 authors 
+          "/books/book[starts-with(@isbn, '1')]">Books, ISBN starts with 1 
+          "/books/book/@isbn[starts-with(., '1')]">ISBNs, starting with 1 
+          "/books/book[contains(title, 'X')]">Books, X in the title 
+          "/books/book[count(authors/author) mod 2 != 0]">Books, even authors 
+          "(/books/book//* | /books/book//@*)[not(self::price)]">All book info except price 
+  */              
+
+
+            }
+            catch (SystemException se)
+            {
+
+            }
+        }
+        static void PrintResults(string XPathExpression)
+        {
+            Console.WriteLine("-------------------------------");
+            XmlNodeList list = doc.SelectNodes(XPathExpression);
+            foreach (XmlNode node in list)
+            {
+                Console.WriteLine("Node Name : " + node.Name + "  " + "Node value : " + node.InnerText); //node.Value will get 
+                                                                                                        //you the attribute and not the element value!
+            }
+
+        }
+    }
+}
