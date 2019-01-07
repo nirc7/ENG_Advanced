d4a4350f24f0fc4f65ea5110d1c4c00d5fb729df
Lab4 RUPPIN <lab4@ruppin.ac.il>
2019-01-07 10:19:42 +0200

    c1

--- 09 ADO I - DataReader/DataReaderDemo/WindowsFormsApp1/Form1.cs
+++ 09 ADO I - DataReader/DataReaderDemo/WindowsFormsApp1/Form1.cs
@@ -13,6 +13,9 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        string conStr = @"Data Source=LAB-400;Initial Catalog=UsersDB;Integrated Security=True";
+
+
         public Form1()
         {
             InitializeComponent();
@@ -20,18 +23,32 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string conStr = @"Data Source=LAB-400;Initial Catalog=UsersDB;Integrated Security=True";
+            ExcNonQ("INSERT INTO UsersTB(Name, Family) VALUES('Dora','Bora')");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExcNonQ("UPDATE UsersTB SET Family='lechlech' WHERE Id=2 ");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExcNonQ("DELETE UsersTB WHERE Id=6 ");
+        }
+
+        private void ExcNonQ(string commndStr)
+        {
             SqlConnection con = new SqlConnection(conStr);
             con.Open();
 
-            SqlCommand com = new SqlCommand("INSERT INTO UsersTB(Name, Family) VALUES('Dora','Bora')", con);
+            SqlCommand com = new SqlCommand(commndStr, con);
             int res = com.ExecuteNonQuery();
-            MessageBox.Show("res="+ res);
+            MessageBox.Show("res=" + res);
 
             con.Close();
-
-
-
         }
+
+       
     }
 }
