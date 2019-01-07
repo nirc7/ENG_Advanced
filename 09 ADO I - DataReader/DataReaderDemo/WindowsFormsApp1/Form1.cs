790e37f20f41b663c55664198814c365fc1d156d
Lab4 RUPPIN <lab4@ruppin.ac.il>
2019-01-07 09:48:47 +0200

    DataReaderDemo

---
+++ 09 ADO I - DataReader/DataReaderDemo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string conStr = @"Data Source=LAB-400;Initial Catalog=UsersDB;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+            con.Open();
+
+            SqlCommand com = new SqlCommand("INSERT INTO UsersTB(Name, Family) VALUES('Dora','Bora')", con);
+            int res = com.ExecuteNonQuery();
+            MessageBox.Show("res="+ res);
+
+            con.Close();
+
+
+
+        }
+    }
+}
